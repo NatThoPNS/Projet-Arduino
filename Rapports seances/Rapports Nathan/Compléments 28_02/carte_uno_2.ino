fec5c737f9841631fb0bf60cd0f588fb977e0a97
NatThoPNS <95168748+NatThoPNS@users.noreply.github.com>
2022-02-28 23:41:26 +0100

    Add files via upload

---
+++ Rapports seances/Rapports Nathan/Compléments 28_02/carte_uno_2.ino
@@ -0,0 +1,79 @@
+// Programme carte Uno 2
+
+//Partie RFID
+#include <SPI.h>
+#include <MFRC522.h>
+const int pinRST = 9; // pin RST du module RC522
+const int pinSDA = 10; // pin SDA du module RC522
+String value;
+MFRC522 rfid(pinSDA, pinRST);
+
+
+//Partie Servo
+#include <Servo.h> // include servo library to use its related functions
+#define Servo_PWM 6 // A descriptive name for D6 pin of Arduino to provide PWM signal
+bool porte_ouverte = false; //on part du principe que la porte est fermée
+Servo MG995_Servo;  // Define an instance of of Servo with the name of "MG995_Servo"
+
+
+void setup(){
+            Serial.begin(9600);
+              
+            //Partie RFID
+            SPI.begin();
+            rfid.PCD_Init();
+          
+            //Partie Servo
+             MG995_Servo.attach(Servo_PWM);  // Connect D6 of Arduino with PWM signal pin of servo motor
+            }
+
+
+void loop(){
+      //Partie RFID
+      Serial.println("En l'attente d'une carte ou d'un badge...");
+      if (rfid.PICC_IsNewCardPresent()){ // on a dédecté un tag
+            if (rfid.PICC_ReadCardSerial()){ // on a lu avec succès son contenu
+                Serial.println("Voici l'UID de ce tag en decimal :");
+                Serial.print("const byte bonUID[");
+                Serial.print(rfid.uid.size);
+                Serial.print("] = {");
+                for (byte i = 0; i < rfid.uid.size; i++){
+                    Serial.print(rfid.uid.uidByte[i]);
+                    value+= String(rfid.uid.uidByte[i]);
+                    if (i < rfid.uid.size - 1){
+                        Serial.print(", ");
+                        }
+                    else{
+                        Serial.println("};");
+                        }
+                    }
+                Serial.print("Value: ");    
+                Serial.println(value);
+                if (value=="715422996"){
+                    Serial.println("Accès accordé.");
+                    if (porte_ouverte==false){
+                          MG995_Servo.write(8); //Turn clockwise at high speed
+                          Serial.println("La porte s'ouvre");
+                          delay(1000);
+                          porte_ouverte = true;                                    
+                          }
+                    else{ //la porte est ouverte
+                       MG995_Servo.write(84); // Turn anti-clockwise
+                       Serial.println("La porte se ferme");
+                       delay(1000);
+                       porte_ouverte = false;
+                       }
+                    delay(3000);
+                    }            
+                else{
+                    Serial.println("Accès refusé.");
+                    //digitalWrite(led_rouge,HIGH);
+                    delay(3000);
+                  }
+
+                Serial.println(); // pour aérer l'affichage du moniteur série
+                value="";  
+                //delay (500); // on patiente un instant avant un nouveau test
+                }
+          }
+      }
